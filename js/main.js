e24ae9af24e739b5fe74b6db32eeb6da2eeb633f
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-07 14:28:32 +0100

    team con una persona

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        immagine: 'wayne-barnett-founder-ceo.jpg'
+    }
+]
+
+console.log(team);
