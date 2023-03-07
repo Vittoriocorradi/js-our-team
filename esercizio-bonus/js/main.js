47014816e6ae08defe3f729c6d069eb1cb925453
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-07 18:18:00 +0100

    Esercizio Bonus finito

--- esercizio-bonus/js/main.js
+++ esercizio-bonus/js/main.js
@@ -42,18 +42,20 @@ const team = [
 const container = document.getElementById('container');
 
 // Ciclo per prendere i singoli elementi della lista
-// for (let i = 0; i < team.length; i++) {
-//     const person = team[i];
-//     console.log(`Membro ${i + 1}`);
-//     const list = document.createElement('ul');
-//     container.append(list);
-//     list.append(`Membro ${i + 1}`);
+for (let i = 0; i < team.length; i++) {
+    console.log(team[i]);
+    const person = team[i];
 
-//     //Ciclo per prendere le singole proprietà dell'oggetto 
-//     for (let key in person) {
-//         console.log(`${key}: ${person[key]}`);
-//         const listElement = document.createElement('li');
-//         listElement.innerHTML = `${key}: ${person[key]}`;
-//         list.append(listElement);
-//     }
-// }
+    // Box membri del team
+    const personBox = document.createElement('div');
+    personBox.classList.add('box');
+    container.append(personBox);
+
+    // Singolo membro del team
+    personBox.innerHTML = `<div class="image"><img src="img/${person['stringa della foto']}"></div><div class="name">${person.nome}</div><div class="role">${person.ruolo}</div>`;
+
+    //Ciclo per prendere le singole proprietà dell'oggetto 
+    for (let key in person) {
+        console.log(`${key}: ${person[key]}`);
+    }
+}
